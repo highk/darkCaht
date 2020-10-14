11920b633b094874009e31a93d07d3524cc69347
HiK <45809984+HighK@users.noreply.github.com>
2020-10-14 23:42:30 +0900

    First Commit

---
+++ backend/src/server.ts
@@ -0,0 +1,62 @@
+import * as express from "express";
+import * as socketIo from "socket.io";
+import * as cors from "cors";
+import * as moment from "moment-timezone";
+import * as path from 'path'
+
+moment.tz.setDefault("Asia/Seoul");
+
+const app = express();
+const server = require("http").createServer(app);
+app.use(cors());
+app.options("*", cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+const io = socketIo(server);
+
+io.on("connection", function (socket: SocketIO.Socket) {
+  console.log("CONNECT: " + socket.id);
+  socket.emit("sysMessage", "SOCKET_CONNECT");
+  socket.broadcast.emit("sysMessage", "New User Join");
+
+  socket.on("message", (data) => {
+    if (data.type === "image") {
+      const url = data.body.match(/http[s]{0,1}:\/\/.+/g);
+      if (url === null) {
+        socket.emit("sysMessage", "URL ERROR");
+        return;
+      }
+
+      io.emit("message", { ...data, body: url });
+      return;
+    }
+
+    if (data.type === "time") {
+      socket.emit("sysMessage", moment().format("YYYY-MM-DD HH:mm:ss"));
+      return;
+    }
+
+    if(data.type === "clear") {
+      socket.emit("command", {type: "clear"})
+      return;
+    }
+
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("DISCONNECT: " + socket.id);
+  });
+});
+
+const PORT = process.env.PORT || "8080";
+
+server.listen(PORT, function () {
+  console.log("Socket IO server listening on port " + PORT);
+});
